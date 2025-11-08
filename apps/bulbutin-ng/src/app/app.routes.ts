e2734b68be16da326713b6099fbc9fa3955afb8e
Lonny Gomes <code@lonnygomes.com>
2025-11-08 15:57:49 -0500

    feat(ng): update routing to have home as the shell

--- apps/bulbutin-ng/src/app/app.routes.ts
+++ apps/bulbutin-ng/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Map } from './map/map';
+import { ImageCardFeed } from './image-card-feed/image-card-feed';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'map', component: Map },
+  {
+    path: '',
+    component: Home,
+    children: [
+      { path: '', redirectTo: 'feed', pathMatch: 'full' },
+      { path: 'feed', component: ImageCardFeed },
+      { path: 'map', component: Map },
+    ],
+  },
   { path: '**', redirectTo: '' },
 ];
--- apps/bulbutin-ng/src/app/image-card-feed/image-card-feed.ts
+++ apps/bulbutin-ng/src/app/image-card-feed/image-card-feed.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ImageCard } from '../image-card/image-card';
+import { ImageFeed } from '../services/image-feed';
 
 @Component({
   selector: 'app-image-card-feed',
-  imports: [],
+  imports: [ImageCard],
   templateUrl: './image-card-feed.html',
   styleUrl: './image-card-feed.scss',
 })
 export class ImageCardFeed {
-
+  private imgFeed = inject(ImageFeed);
+  images = this.imgFeed.images;
 }
